ab0d41c6060eb73b58d92f4344b6dca2d4e3a642
paulyarin <pavyarin@yandex.ru>
2021-05-30 15:39:02 +0700

    Добавил проверку на положительные числа и границы диапазонов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 //Cлучайное целое число в заданном диапазоне
 function getRandomNumber (min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  min = Math.abs(Math.ceil(min));
+  max = Math.abs(Math.floor(max));
+  if (min >= max) {
+    return 'Верхняя граница диапазона должна быть больше, чем меньшая!';
+  }
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-getRandomNumber(500, 400);
+getRandomNumber(100, 200);
