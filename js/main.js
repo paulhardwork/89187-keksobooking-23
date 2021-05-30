4477b15ea330fcba3cfb0557c85f20a15abd630b
paulyarin <pavyarin@yandex.ru>
2021-05-30 23:52:19 +0700

    Небольшой рефакторинг кода первой функции

--- js/main.js
+++ js/main.js
@@ -2,10 +2,7 @@
 function getRandomNumber (min, max) {
   min = Math.abs(Math.ceil(min));
   max = Math.abs(Math.floor(max));
-  if (min >= max) {
-    return 'Верхняя граница диапазона должна быть больше, чем меньшая!';
-  }
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return min >= max ? 'Верхняя граница диапазона должна быть больше, чем меньшая!' : Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomNumber(100, 200);
 
