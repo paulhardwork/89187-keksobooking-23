4844792969251976a24246445bc12e4b7785ed81
paulyarin <pavyarin@yandex.ru>
2021-06-14 15:20:56 +0700

    Привел содержание точки входа к актуальному состоянию

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-import './util.js';
+import './util.js'; // Насколько это нужно здесь? Кода у нас не выполняется в нем, но ведь в точке входа в любом случае надо все модули обозначить?
 import {createAdvert} from './data.js';
 
 const ADVERTS_QUANTITY = 10;
 const advertsList = new Array(ADVERTS_QUANTITY).fill(null).map((__, index) => createAdvert(index));
 advertsList.length; // Временное использование массива
-console.log(advertsList);
