35c29a195b8f408cf3cbbae6bae02ea27e5cac1b
paulyarin <pavyarin@yandex.ru>
2021-06-22 17:30:13 +0700

    Начал отрисовку объявлений в контейнер карты

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 import {createAdvert} from './data.js';
 
-const ADVERTS_QUANTITY = 8;
+const ADVERTS_QUANTITY = 10;
 const getAdvertsList = function (advertsCount) {
   return new Array(advertsCount).fill(null).map((__, index) => createAdvert(index));
 };
 
-getAdvertsList(ADVERTS_QUANTITY);
+
+const mapContainer = document.querySelector('.map');
+const mapCanvas = mapContainer.querySelector('#map-canvas');
+const similarAdvertsTemplate = document.querySelector('#card').content;
+
+const similarAdverts = getAdvertsList(ADVERTS_QUANTITY);
+
+similarAdverts.forEach((advert) => {
+  const similarAdvert = similarAdvertsTemplate.cloneNode(true);
+  similarAdvert.querySelector('.popup__title').textContent = advert.offer.title;
+  mapCanvas.appendChild(similarAdvert);
+});
