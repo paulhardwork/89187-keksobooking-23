116ad1bb2ec7dd3fb942ac26e345f423584af5bc
paulyarin <pavyarin@yandex.ru>
2021-06-22 16:30:05 +0700

    Создал функцию для генерации объявлений

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-import './util.js'; // Насколько это нужно здесь? Кода у нас не выполняется в нем, но ведь в точке входа в любом случае надо все модули обозначить?
 import {createAdvert} from './data.js';
 
-const ADVERTS_QUANTITY = 10;
-const advertsList = new Array(ADVERTS_QUANTITY).fill(null).map((__, index) => createAdvert(index));
-advertsList.length; // Временное использование массива
+const ADVERTS_QUANTITY = 8;
+const getAdvertsList = function (advertsCount) {
+  return new Array(advertsCount).fill(null).map((__, index) => createAdvert(index));
+};
+
+getAdvertsList(ADVERTS_QUANTITY);
