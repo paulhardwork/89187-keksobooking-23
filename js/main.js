282ab58b43e4b44d6698d4c56b0f04f721a3b318
paulyarin <pavyarin@yandex.ru>
2021-07-02 00:53:22 +0700

    Вынесены основные вызовы и импорты

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-import './adverts.js';
+import {getAdvertsList} from './data.js';
+import {getNewAdvert} from './adverts.js';
+
+const ADVERTS_QUANTITY = 10;
+const newAdverts = getAdvertsList(ADVERTS_QUANTITY);
+
+const mapContainer = document.querySelector('.map');
+const mapCanvas = mapContainer.querySelector('#map-canvas');
+
+mapCanvas.appendChild(getNewAdvert(newAdverts[5]));
