1283594eae34ff43adb71dea45a1e2df64ba081a
paulyarin <pavyarin@yandex.ru>
2021-07-08 22:48:38 +0700

    Добавлены функции активации и деактивации страницы

--- js/form.js
+++ js/form.js
@@ -1,49 +1,71 @@
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
-const MAX_PRICE = 1000000;
-const SELECTED_QUANTITY_ROOMS = 1;
-const ROOMS_NOT_GUESTS = '100';
-const VALUE_FOR_HUNDRED_ROOMS = '0';
+const addingAdvertForm = document.querySelector('.ad-form');
+const addingAdvertFields = addingAdvertForm.querySelectorAll('fieldset');
+const filterAdvertsForm = document.querySelector('.map__filters');
+const filterAdvertsFields = filterAdvertsForm.children;
 
-const advertTitleInput = document.querySelector('#title');
-const priceInput = document.querySelector('#price');
-const selecterRoomNumber = document.querySelector('#room_number');
-const selecterGuestQuantity = document.querySelector('#capacity');
-const optionsCapacity = selecterGuestQuantity.querySelectorAll('option');
-
-const toggleOptionsCapacity = function (valueRooms) {
-  for (let i = 0; i < optionsCapacity.length; i++) {
-    if (valueRooms === ROOMS_NOT_GUESTS) {
-      optionsCapacity[i].value !== VALUE_FOR_HUNDRED_ROOMS ? optionsCapacity[i].disabled = true : optionsCapacity[i].disabled = false;
-    } else {
-      optionsCapacity[i].value > selecterRoomNumber.value || optionsCapacity[i].value === VALUE_FOR_HUNDRED_ROOMS ? optionsCapacity[i].disabled = true : optionsCapacity[i].disabled = false;
-    }
-  }
+const activateDocument = function () {
+  addingAdvertForm.classList.remove('ad-form--disabled');
+  addingAdvertFields.disabled = false;
+  filterAdvertsForm.classList.remove('map__filters--disabled');
+  filterAdvertsFields.disabled = false;
 };
 
-advertTitleInput.addEventListener('input', () => {
-  const valueLength = advertTitleInput.value.length;
-  if (valueLength < MIN_TITLE_LENGTH) {
-    advertTitleInput.setCustomValidity('Минимальное значение - 30 символов');
-  } else if (valueLength > MAX_TITLE_LENGTH) {
-    advertTitleInput.setCustomValidity('Длина не должна превышать 100 символов!');
-  } else {
-    advertTitleInput.setCustomValidity('');
-  }
-  advertTitleInput.reportValidity();
-});
+const deactivateDocument = function () {
+  addingAdvertForm.classList.add('ad-form--disabled');
+  addingAdvertFields.disabled = true;
+  filterAdvertsForm.classList.add('map__filters--disabled');
+  filterAdvertsFields.disabled = true;
+};
 
-priceInput.addEventListener('invalid', () => {
-  if (priceInput.value > MAX_PRICE) {
-    priceInput.setCustomValidity('Цена не может превышать 1000000 рублей за ночь!');
-  } else {
-    priceInput.setCustomValidity('');
-  }
-});
+const initForm = function () {
+  const MIN_TITLE_LENGTH = 30;
+  const MAX_TITLE_LENGTH = 100;
+  const MAX_PRICE = 1000000;
+  const SELECTED_QUANTITY_ROOMS = 1;
+  const ROOMS_NOT_GUESTS = '100';
+  const VALUE_FOR_HUNDRED_ROOMS = '0';
 
-toggleOptionsCapacity(SELECTED_QUANTITY_ROOMS);
+  const advertTitleInput = document.querySelector('#title');
+  const priceInput = document.querySelector('#price');
+  const selecterRoomNumber = document.querySelector('#room_number');
+  const selecterGuestQuantity = document.querySelector('#capacity');
+  const optionsCapacity = selecterGuestQuantity.querySelectorAll('option');
 
-selecterRoomNumber.addEventListener('change', () => {
-  toggleOptionsCapacity(selecterRoomNumber.value);
-});
+  const toggleOptionsCapacity = function (valueRooms) {
+    for (let i = 0; i < optionsCapacity.length; i++) {
+      if (valueRooms === ROOMS_NOT_GUESTS) {
+        optionsCapacity[i].value !== VALUE_FOR_HUNDRED_ROOMS ? optionsCapacity[i].disabled = true : optionsCapacity[i].disabled = false;
+      } else {
+        optionsCapacity[i].value > selecterRoomNumber.value || optionsCapacity[i].value === VALUE_FOR_HUNDRED_ROOMS ? optionsCapacity[i].disabled = true : optionsCapacity[i].disabled = false;
+      }
+    }
+  };
 
+  advertTitleInput.addEventListener('input', () => {
+    const valueLength = advertTitleInput.value.length;
+    if (valueLength < MIN_TITLE_LENGTH) {
+      advertTitleInput.setCustomValidity('Минимальное значение - 30 символов');
+    } else if (valueLength > MAX_TITLE_LENGTH) {
+      advertTitleInput.setCustomValidity('Длина не должна превышать 100 символов!');
+    } else {
+      advertTitleInput.setCustomValidity('');
+    }
+    advertTitleInput.reportValidity();
+  });
+
+  priceInput.addEventListener('invalid', () => {
+    if (priceInput.value > MAX_PRICE) {
+      priceInput.setCustomValidity('Цена не может превышать 1000000 рублей за ночь!');
+    } else {
+      priceInput.setCustomValidity('');
+    }
+  });
+
+  toggleOptionsCapacity(SELECTED_QUANTITY_ROOMS);
+
+  selecterRoomNumber.addEventListener('change', () => {
+    toggleOptionsCapacity(selecterRoomNumber.value);
+  });
+};
+
+export {initForm, deactivateDocument, activateDocument};
