51167e094d8f843e8465f516184bb8855768ed87
paulyarin <pavyarin@yandex.ru>
2021-07-11 16:12:59 +0700

    Реализовано до конца добавление похожих объявлений с балуном на карту

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-import {getAdvertsList} from './data.js';
-import {getNewAdvert} from './adverts.js';
 import {initForm} from './form.js';
-import './map.js';
+import {initMap} from './map.js';
 
+initMap();
 initForm();
-
-const ADVERTS_QUANTITY = 10;
-const newAdverts = getAdvertsList(ADVERTS_QUANTITY);
-const mapContainer = document.querySelector('.map');
-const mapCanvas = mapContainer.querySelector('#map-canvas');
 
 
--- js/map.js
+++ js/map.js
@@ -2,74 +2,84 @@ import {getNewAdvert} from './adverts.js';
 import {getAdvertsList} from './data.js';
 import {activateDocument, deactivateDocument} from './form.js';
 
-deactivateDocument();
-const addressField = document.querySelector('#address');
-const cityMap = L.map('map-canvas')
-  .on('load', () => {
-    activateDocument();
-  })
-  .setView({
-    lat: 35.68950,
-    lng: 139.69171,
-  }, 10);
+const initMap = function () {
+  deactivateDocument();
+  const LAT_TOKYO = 35.68950;
+  const LNG_TOKYO = 139.69171;
+  const addressField = document.querySelector('#address');
+  const cityMap = L.map('map-canvas')
+    .on('load', () => {
+      activateDocument();
+    })
+    .setView({
+      lat: LAT_TOKYO,
+      lng: LNG_TOKYO,
+    }, 10);
 
-addressField.value = '35.68950, 139.69171';
+  addressField.value = `${LAT_TOKYO}, ${LNG_TOKYO}`;
 
-L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  },
-).addTo(cityMap);
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    },
+  ).addTo(cityMap);
 
-const mainPinIcon = L.icon({
-  iconUrl: './img/main-pin.svg',
-  iconSize: [52, 52],
-  iconAnchor: [26, 52],
-});
-
-const mainMarker = L.marker(
-  {
-    lat: 35.68950,
-    lng: 139.69171,
-  },
-  {
-    draggable: true,
-    icon: mainPinIcon,
+  const mainPinIcon = L.icon({
+    iconUrl: './img/main-pin.svg',
+    iconSize: [52, 52],
+    iconAnchor: [26, 52],
   });
 
-mainMarker.addTo(cityMap);
-mainMarker.on('moveend', (evt) => {
-  const currentLocation = evt.target.getLatLng();
-  addressField.value = `${Number(currentLocation.lat.toFixed(5))}, ${Number(currentLocation.lng.toFixed(5))}`;
-});
+  const mainMarker = L.marker(
+    {
+      lat: 35.68950,
+      lng: 139.69171,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    });
 
-const ADVERTS_QUANTITY = 10;
-const newAdverts = getAdvertsList(ADVERTS_QUANTITY);
-
-newAdverts.forEach((advert) => {
-  const lat = advert.location.lat;
-  const lng = advert.location.lng;
-
-  const similarAdvertIcon = L.icon({
-    iconUrl: './img/pin.svg',
-    iconSize: [40, 40],
-    iconAnchor: [20, 40],
+  mainMarker.addTo(cityMap);
+  mainMarker.on('moveend', (evt) => {
+    const currentLocation = evt.target.getLatLng();
+    addressField.value = `${Number(currentLocation.lat.toFixed(5))}, ${Number(currentLocation.lng.toFixed(5))}`;
   });
 
-  const similarAdvertMarker = L.marker(
-    {
-      lat,
-      lng,
-    },
-    {
-      icon: similarAdvertIcon,
-    },
-  );
+  const ADVERTS_QUANTITY = 10;
+  const newAdverts = getAdvertsList(ADVERTS_QUANTITY);
 
-  similarAdvertMarker
-    .addTo(cityMap)
-    .bindPopup(
-      getNewAdvert(advert),
+  const createAdvertMarker = function (advert) {
+    const lat = advert.location.lat;
+    const lng = advert.location.lng;
+
+    const similarAdvertIcon = L.icon({
+      iconUrl: './img/pin.svg',
+      iconSize: [40, 40],
+      iconAnchor: [20, 40],
+    });
+
+    const similarAdvertMarker = L.marker(
+      {
+        lat,
+        lng,
+      },
+      {
+        icon: similarAdvertIcon,
+      },
     );
-});
+
+    similarAdvertMarker
+      .addTo(cityMap)
+      .bindPopup(
+        getNewAdvert(advert),
+      );
+  };
+
+  newAdverts.forEach((advert) => {
+    createAdvertMarker(advert);
+  });
+};
+
+export {initMap};
