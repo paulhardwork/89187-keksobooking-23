fb7ed68a5d260918a6f12cb315bb1bc1a2661166
paulyarin <pavyarin@yandex.ru>
2021-07-18 21:08:20 +0700

    Добавлена функция для инициализации формы

--- js/form.js
+++ js/form.js
@@ -8,8 +8,13 @@ const MAX_PRICE = 1000000;
 const SELECTED_QUANTITY_ROOMS = 1;
 const ROOMS_NOT_GUESTS = 100;
 const VALUE_FOR_HUNDRED_ROOMS = 0;
+const MIN_PRICE_BUNGALOW = 0;
+const MIN_PRICE_FLAT = 1000;
+const MIN_PRICE_HOTEL = 3000;
+const MIN_PRICE_HOUSE = 5000;
+const MIN_PRICE_PALACE = 10000;
 
-const RANGE = {min: 10000, max: 50000};
+const Range = {MIN: 10000, MAX: 50000};
 
 const filterAdvertsForm = document.querySelector('.map__filters');
 const filterAdvertsFields = filterAdvertsForm.children;
@@ -48,13 +53,7 @@ const deactivateDocument = function () {
 };
 
 const successSendingMessage = document.querySelector('#success').content.querySelector('.success');
-document.body.append(successSendingMessage);
-successSendingMessage.classList.add('hidden');
-
 const errorSendingMessage = document.querySelector('#error').content.querySelector('.error');
-document.body.append(errorSendingMessage);
-errorSendingMessage.classList.add('hidden');
-
 const errorMessageButton = errorSendingMessage.querySelector('.error__button');
 
 const hideFormMessages = function () {
@@ -65,79 +64,87 @@ const hideFormMessages = function () {
   }
 };
 
-const toggleOptionsCapacity = function (valueRooms) {
-  for (let i = 0; i < optionsCapacity.length; i++) {
-    if (valueRooms === ROOMS_NOT_GUESTS.toString()) {
-      optionsCapacity[i].disabled = optionsCapacity[i].value !== VALUE_FOR_HUNDRED_ROOMS.toString();
-      selecterGuestQuantity.value = VALUE_FOR_HUNDRED_ROOMS.toString();
-    } else {
-      optionsCapacity[i].disabled = optionsCapacity[i].value > selecterRoomNumber.value || optionsCapacity[i].value === VALUE_FOR_HUNDRED_ROOMS.toString();
-      selecterGuestQuantity.value = SELECTED_QUANTITY_ROOMS.toString();
-    }
-  }
-};
+const initForm = function () {
+  document.body.append(successSendingMessage);
+  successSendingMessage.classList.add('hidden');
 
-advertTitleInput.addEventListener('input', () => {
-  const valueLength = advertTitleInput.value.length;
-  if (valueLength < MIN_TITLE_LENGTH) {
-    advertTitleInput.setCustomValidity('Минимальное значение - 30 символов');
-  } else if (valueLength > MAX_TITLE_LENGTH) {
-    advertTitleInput.setCustomValidity('Длина не должна превышать 100 символов!');
-  } else {
-    advertTitleInput.setCustomValidity('');
-  }
-  advertTitleInput.reportValidity();
-});
-
-selecterHouseType.addEventListener('change', () => {
-  if (selecterHouseType.value === 'bungalow') {
-    priceInput.min = 0;
-    priceInput.placeholder = 0;
-  } else if (selecterHouseType.value === 'flat') {
-    priceInput.min = 1000;
-    priceInput.placeholder = 1000;
-  } else if (selecterHouseType.value === 'hotel') {
-    priceInput.min = 3000;
-    priceInput.placeholder = 3000;
-  } else if (selecterHouseType.value === 'house') {
-    priceInput.min = 5000;
-    priceInput.placeholder = 5000;
-  } else if (selecterHouseType.value === 'palace') {
-    priceInput.min = 10000;
-    priceInput.placeholder = 10000;
-  }
-});
-
-selecterTimeIn.addEventListener('change', () => selecterTimeOut.value = selecterTimeIn.value);
-selecterTimeOut.addEventListener('change', () => selecterTimeIn.value = selecterTimeOut.value);
-
-priceInput.addEventListener('invalid', () => {
-  if (priceInput.value > MAX_PRICE) {
-    priceInput.setCustomValidity(`Цена не может превышать ${MAX_PRICE} рублей за ночь!`);
-  } else {
-    priceInput.setCustomValidity('');
-  }
-});
-
-toggleOptionsCapacity(SELECTED_QUANTITY_ROOMS);
-
-selecterRoomNumber.addEventListener('change', () => {
-  toggleOptionsCapacity(selecterRoomNumber.value);
-});
-
-document.addEventListener('click', () => {
-  hideFormMessages();
-});
-
-document.addEventListener('keydown', (evt) => {
-  if( isEscEvent(evt) ) {
-    hideFormMessages();
-  }
-});
-
-errorMessageButton.addEventListener('click', () => {
+  document.body.append(errorSendingMessage);
   errorSendingMessage.classList.add('hidden');
-});
+
+  const toggleOptionsCapacity = function (valueRooms) {
+    for (let i = 0; i < optionsCapacity.length; i++) {
+      if (valueRooms === ROOMS_NOT_GUESTS.toString()) {
+        optionsCapacity[i].disabled = optionsCapacity[i].value !== VALUE_FOR_HUNDRED_ROOMS.toString();
+        selecterGuestQuantity.value = VALUE_FOR_HUNDRED_ROOMS.toString();
+      } else {
+        optionsCapacity[i].disabled = optionsCapacity[i].value > selecterRoomNumber.value || optionsCapacity[i].value === VALUE_FOR_HUNDRED_ROOMS.toString();
+        selecterGuestQuantity.value = SELECTED_QUANTITY_ROOMS.toString();
+      }
+    }
+  };
+
+  advertTitleInput.addEventListener('input', () => {
+    const valueLength = advertTitleInput.value.length;
+    if (valueLength < MIN_TITLE_LENGTH) {
+      advertTitleInput.setCustomValidity(`Минимальное значение - ${MIN_TITLE_LENGTH} символов'`);
+    } else if (valueLength > MAX_TITLE_LENGTH) {
+      advertTitleInput.setCustomValidity(`Длина не должна превышать ${MAX_TITLE_LENGTH} символов!`);
+    } else {
+      advertTitleInput.setCustomValidity('');
+    }
+    advertTitleInput.reportValidity();
+  });
+
+  selecterHouseType.addEventListener('change', () => {
+    if (selecterHouseType.value === 'bungalow') {
+      priceInput.min = MIN_PRICE_BUNGALOW;
+      priceInput.placeholder = MIN_PRICE_BUNGALOW;
+    } else if (selecterHouseType.value === 'flat') {
+      priceInput.min = MIN_PRICE_FLAT;
+      priceInput.placeholder = MIN_PRICE_FLAT;
+    } else if (selecterHouseType.value === 'hotel') {
+      priceInput.min = MIN_PRICE_HOTEL;
+      priceInput.placeholder = MIN_PRICE_HOTEL;
+    } else if (selecterHouseType.value === 'house') {
+      priceInput.min = MIN_PRICE_HOUSE;
+      priceInput.placeholder = MIN_PRICE_HOUSE;
+    } else if (selecterHouseType.value === 'palace') {
+      priceInput.min = MIN_PRICE_PALACE;
+      priceInput.placeholder = MIN_PRICE_PALACE;
+    }
+  });
+
+  selecterTimeIn.addEventListener('change', () => selecterTimeOut.value = selecterTimeIn.value);
+  selecterTimeOut.addEventListener('change', () => selecterTimeIn.value = selecterTimeOut.value);
+
+  priceInput.addEventListener('invalid', () => {
+    if (priceInput.value > MAX_PRICE) {
+      priceInput.setCustomValidity(`Цена не может превышать ${MAX_PRICE} рублей за ночь!`);
+    } else {
+      priceInput.setCustomValidity('');
+    }
+  });
+
+  toggleOptionsCapacity(SELECTED_QUANTITY_ROOMS);
+
+  selecterRoomNumber.addEventListener('change', () => {
+    toggleOptionsCapacity(selecterRoomNumber.value);
+  });
+
+  document.addEventListener('click', () => {
+    hideFormMessages();
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if( isEscEvent(evt) ) {
+      hideFormMessages();
+    }
+  });
+
+  errorMessageButton.addEventListener('click', () => {
+    errorSendingMessage.classList.add('hidden');
+  });
+};
 
 const setResetAdvertForm = function (onReset) {
   resetFormButton.addEventListener('click', (evt) => {
@@ -185,11 +192,11 @@ const filterSimilarAdverts = function (advert) {
   }
 
   if (choosenPrice !== 'any') {
-    if (advertPrice < RANGE.min) {
+    if (advertPrice < Range.MIN) {
       advertPriceString = 'low';
-    } else if (advertPrice > RANGE.min && advertPrice < RANGE.max) {
+    } else if (advertPrice > Range.MIN && advertPrice < Range.MAX) {
       advertPriceString = 'middle';
-    } else if (advertPrice > RANGE.max){
+    } else if (advertPrice > Range.MAX){
       advertPriceString = 'high';
     }
     isPrice = choosenPrice === advertPriceString;
@@ -214,4 +221,4 @@ const getFilterChange = function (afterChange) {
   });
 };
 
-export {deactivateDocument, activateDocument, filterSimilarAdverts, getFilterChange, activateFiltersForm, setSubmitAdvertForm, setResetAdvertForm, successSendingMessage, errorSendingMessage};
+export {initForm, deactivateDocument, activateDocument, filterSimilarAdverts, getFilterChange, activateFiltersForm, setSubmitAdvertForm, setResetAdvertForm, successSendingMessage, errorSendingMessage};
